60d77409d72e8313625a8328da353be2984105b5
Theo Madden-Abbott <theo@madden-abbott.com>
2015-03-04 18:30:32 +0000

    Added simple list view.

--- app/src/main/java/com/madden_abbott/shoppinglist/ListsActivity.java
+++ app/src/main/java/com/madden_abbott/shoppinglist/ListsActivity.java
@@ -1,39 +1,25 @@
 package com.madden_abbott.shoppinglist;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
-public class ListsActivity extends ActionBarActivity {
+public class ListsActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lists);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lists, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        String[] values = new String[]{
+                "Hello", "Goodbye"
+        };
+        ListAdapter adapter = new ArrayAdapter<>(this, R.layout.simple_list_item, values);
+        ListView listView = new ListView(this);
+        listView.setAdapter(adapter);
+        setContentView(listView);
     }
 }
